014ac9e85f314e5a33b8bc41fbbf8d57c17d4d4c
Rishab038 <87517811+Rishab038@users.noreply.github.com>
2024-08-05 13:46:34 +0530

    auth/login error

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -80,7 +80,7 @@ const Form = () => {
 
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch("http://localhost:3001/auth/login", {
-      method: "POST",
+      method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(values),
     });
